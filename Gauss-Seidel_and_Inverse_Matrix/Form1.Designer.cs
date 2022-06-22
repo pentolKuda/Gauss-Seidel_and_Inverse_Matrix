60d8460086766fe596de1f303eb4c76a401c69b7
F_Zamzami <68177295+pentolKuda@users.noreply.github.com>
2022-06-22 13:20:53 +0800

    first commit

---
+++ Gauss-Seidel_and_Inverse_Matrix/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Gauss_Seidel_and_Inverse_Matrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbGauss = new System.Windows.Forms.RadioButton();
+            this.rbInverse = new System.Windows.Forms.RadioButton();
+            this.tbInputNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupEquation = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cbOk = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupEquation.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbInverse);
+            this.groupBox1.Controls.Add(this.rbGauss);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 118);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Method";
+            // 
+            // rbGauss
+            // 
+            this.rbGauss.AutoSize = true;
+            this.rbGauss.Location = new System.Drawing.Point(28, 35);
+            this.rbGauss.Name = "rbGauss";
+            this.rbGauss.Size = new System.Drawing.Size(114, 21);
+            this.rbGauss.TabIndex = 0;
+            this.rbGauss.TabStop = true;
+            this.rbGauss.Text = "Gauss-Seidel";
+            this.rbGauss.UseVisualStyleBackColor = true;
+            // 
+            // rbInverse
+            // 
+            this.rbInverse.AutoSize = true;
+            this.rbInverse.Location = new System.Drawing.Point(28, 61);
+            this.rbInverse.Name = "rbInverse";
+            this.rbInverse.Size = new System.Drawing.Size(116, 21);
+            this.rbInverse.TabIndex = 1;
+            this.rbInverse.TabStop = true;
+            this.rbInverse.Text = "Inverse Matrix";
+            this.rbInverse.UseVisualStyleBackColor = true;
+            // 
+            // tbInputNumber
+            // 
+            this.tbInputNumber.Location = new System.Drawing.Point(121, 33);
+            this.tbInputNumber.Name = "tbInputNumber";
+            this.tbInputNumber.Size = new System.Drawing.Size(100, 22);
+            this.tbInputNumber.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbOk);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbInputNumber);
+            this.groupBox2.Location = new System.Drawing.Point(280, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(242, 118);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equation Setup";
+            // 
+            // groupEquation
+            // 
+            this.groupEquation.Controls.Add(this.dataGridView2);
+            this.groupEquation.Controls.Add(this.dataGridView1);
+            this.groupEquation.Controls.Add(this.button5);
+            this.groupEquation.Controls.Add(this.button1);
+            this.groupEquation.Controls.Add(this.button2);
+            this.groupEquation.Controls.Add(this.label6);
+            this.groupEquation.Controls.Add(this.label5);
+            this.groupEquation.Location = new System.Drawing.Point(12, 170);
+            this.groupEquation.Name = "groupEquation";
+            this.groupEquation.Size = new System.Drawing.Size(607, 533);
+            this.groupEquation.TabIndex = 7;
+            this.groupEquation.TabStop = false;
+            this.groupEquation.Text = "Equation";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matrix Size Equation:\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "number";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Solve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cbOk
+            // 
+            this.cbOk.AutoSize = true;
+            this.cbOk.Location = new System.Drawing.Point(121, 76);
+            this.cbOk.Name = "cbOk";
+            this.cbOk.Size = new System.Drawing.Size(50, 21);
+            this.cbOk.TabIndex = 5;
+            this.cbOk.Text = "OK";
+            this.cbOk.UseVisualStyleBackColor = true;
+            this.cbOk.CheckedChanged += new System.EventHandler(this.cbOk_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 25);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(327, 400);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 25);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "delete";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(316, 314);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(386, 68);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(193, 314);
+            this.dataGridView2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 733);
+            this.Controls.Add(this.groupEquation);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "System Linear Equation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupEquation.ResumeLayout(false);
+            this.groupEquation.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbInverse;
+        private System.Windows.Forms.RadioButton rbGauss;
+        private System.Windows.Forms.TextBox tbInputNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupEquation;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cbOk;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Gauss-Seidel_and_Inverse_Matrix/Form1.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gauss_Seidel_and_Inverse_Matrix
+{
+    public partial class Form1 : Form
+    {
+
+        int matrixSize;
+        static int N;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void cbOk_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbOk.Checked)
+            {
+                try
+                {
+                    matrixSize = int.Parse(tbInputNumber.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Masukkan nilai dengan benar!");
+                    cbOk.Checked = false;
+                    return;
+                }
+                tbInputNumber.ReadOnly = true;
+                          
+                label6.Text = matrixSize.ToString() + " X " + matrixSize.ToString();
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView2.Rows.Clear();
+                dataGridView2.Columns.Clear();
+
+                var colX1 = new DataGridViewColumn();
+                colX1.HeaderText = "answer";
+                colX1.CellTemplate = new DataGridViewTextBoxCell();
+                colX1.Width = 70;
+                dataGridView2.Columns.Add(colX1);
+
+
+
+                for(int i=0; i < matrixSize + 1; i++)
+                {
+                    var column = new DataGridViewColumn();
+                    column.HeaderText = (i == matrixSize ? "Y" : "X" + (i + 1).ToString());
+                    column.CellTemplate = new DataGridViewTextBoxCell();
+                    column.Width = 30;
+                    dataGridView1.Columns.Add(column);
+
+                    if(i != matrixSize)
+                    {
+                        dataGridView2.Rows.Add();
+                    }
+                }
+
+                for(int j=0; j<matrixSize; j++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+
+
+            }
+            else
+            {
+                tbInputNumber.ReadOnly = false;
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rbGauss.Checked)
+            {
+                double[][] x_eq = new double[matrixSize][];
+                double[] y_eq = new double[matrixSize];
+                double[] r_eq = new double[matrixSize];
+
+                for(int i = 0; i < matrixSize; i++)
+                {
+                    x_eq[i] = new double[matrixSize];
+                    r_eq[i] = 0;
+                    y_eq[i] = double.Parse((string) dataGridView1.Rows[i].Cells[matrixSize].Value);
+                    for(int j=0; j < matrixSize; j++)
+                    {
+                        x_eq[i][j] = double.Parse((string)dataGridView1.Rows[i].Cells[j].Value);
+                        //Console.Write(x_eq[i][j] + "\t");
+                    }
+                    //Console.WriteLine();
+                }
+
+                r_eq = GaussSeidel(x_eq, y_eq, r_eq);
+
+                if (Double.IsNaN(r_eq[0])){
+                    MessageBox.Show("Doesn't convergence");
+                }
+
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    Console.Write(r_eq[i] + "\t");
+                    dataGridView2.Rows[i].Cells[0].Value = r_eq[i];
+                }
+            }
+            else if(rbInverse.Checked)
+            {
+                double[] y_eq = new double[matrixSize];
+                double[] r_eq = new double[matrixSize];
+
+                double[,] x_eq = new double[matrixSize,matrixSize];
+                double[,] inv_x_eq = new double[matrixSize,matrixSize];
+
+
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    y_eq[i] = double.Parse((string)dataGridView1.Rows[i].Cells[matrixSize].Value);
+                    for (int j = 0; j < matrixSize; j++)
+                    {
+                        x_eq[i,j] = double.Parse((string)dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+                //inv_x_eq = InverseMatrix(x_eq);
+                N = x_eq.GetLength(0);
+                //Console.WriteLine("N", N);
+                inverse(x_eq, inv_x_eq);
+                display(x_eq);
+                Console.WriteLine();
+                display(inv_x_eq);
+
+                for(int i=0; i<matrixSize; i++)
+                {
+                    
+                    for(int j=0; j<matrixSize; j++)
+                    {
+                        r_eq[i] += inv_x_eq[i, j] * y_eq[j];
+                    }
+                    Console.WriteLine(r_eq[i]);
+                }
+
+                for (int i = 0; i < matrixSize; i++)
+                {
+                    Console.Write(r_eq[i] + "\t");
+                    dataGridView2.Rows[i].Cells[0].Value = r_eq[i];
+                }
+
+
+
+            }
+        }
+
+
+        public static double MaxError = .00000001;
+        public static double MaxIteration = 1000000;
+        public static double Lambda = .5;
+        public static double[] GaussSeidel1(double[][] a, double[] b, double[] r)
+        {
+            int n = a.Length;
+            double[] r_prev = new double[n];
+            //declare r_prev to zero
+            for(int i =0; i < n; i++)
+            {
+                r_prev[i] = 0.0;
+            }
+
+            for(int i=0; i < MaxIteration; i++) {
+                for(int j=0; j < n; j++)
+                {
+                    r_prev[j] = r[j];
+                }
+
+                for(int j=0; j<n; j++)
+                {
+                    double sum = 0.0;
+                    for(int k=0; k<n; k++)
+                    {
+                        if (k != j)
+                        {
+                            sum = sum + a[j][k] * r[k];
+                        }
+                    }
+                    r[j] = (b[j] - sum) / a[j][j];
+
+                }
+
+                double diff1_norm = 0.0;
+                double old_norm = 0.0;
+                for(int j=0; j < n; j++)
+                {
+                    diff1_norm = diff1_norm + Math.Abs(r[j] - r_prev[j]);
+                    old_norm = old_norm + Math.Abs(r_prev[j]);
+                }
+                if(old_norm == 0.0)
+                {
+                    old_norm = 1.0;
+                }
+                double norm = diff1_norm / old_norm;
+                if(norm<MaxError && i != 0)
+                {
+                    //Console.Write("Sequence converges to [");
+                    for(int j=0; j<n-1; j++)
+                    {
+                        //Console.Write(r[j]);
+                    }
+                    //Console.Write(r[n - 1]);
+                    //Console.Write(" ]. Took ");
+                    //Console.Write(i + 1);
+                    //Console.WriteLine(" iterations.");
+                    return r;
+                }
+            }
+            Console.WriteLine("Doesnt converge");
+            for(int i=0; i < n; i++)
+            {
+                r[i] = double.NaN;
+            }
+            return r;
+        }
+
+        public static double[] GaussSeidel(double[][] a, double[] b, double[] r)
+        {
+            int n = a.Length;
+
+            //Division by the diagonal element to reduce calculation
+            for (int i = 0; i < n; i++)
+            {
+                double d = a[i][i];
+                for (int j = 0; j < n; j++)
+                {
+                    a[i][j] = a[i][j] / d;
+                }
+                b[i] = b[i] / d;
+            }
+
+            //Generation of initial values for roots
+            for (int i = 0; i < n; i++)
+            {
+                double sum = b[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        sum -= (a[i][j] * r[j]);
+                    }
+                }
+                r[i] = sum;
+            }
+
+            //Iterations for converging to the real roots
+            for (int itr = 1; itr < MaxIteration; itr++)
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+                    double old = r[i];
+                    double sum = b[i];
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i != j)
+                            sum -= (a[i][j] * r[j]);
+                    }
+
+                    r[i] = Lambda * sum + (1 - Lambda) * old;
+                    if (r[i] != 0)
+                    {
+                        double ea = Math.Abs((r[i] - old) / r[i]) * 100;
+                        if (ea < MaxError)
+                            return r;
+                    }
+                }
+            }
+
+            return r;
+        }
+
+
+        //==============================================================
+
+        static void getCofactor(double[,] A, double[,] temp, int p, int q, int n)
+        {
+            int i = 0, j = 0;
+
+            // Looping for each element of the matrix
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    // Copying into temporary matrix only those element
+                    // which are not in given row and column
+                    if (row != p && col != q)
+                    {
+                        temp[i, j++] = A[row, col];
+
+                        // Row is filled, so increase row index and
+                        // reset col index
+                        if (j == n - 1)
+                        {
+                            j = 0;
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        static double determinant(double[,] A, int n)
+        {
+            double D = 0; // Initialize result
+
+            // Base case : if matrix contains single element
+            if (n == 1)
+                return A[0, 0];
+
+            double[,] temp = new double[N, N]; // To store cofactors
+
+            int sign = 1; // To store sign multiplier
+
+            // Iterate for each element of first row
+            for (int f = 0; f < n; f++)
+            {
+                // Getting Cofactor of A[0,f]
+                getCofactor(A, temp, 0, f, n);
+                D += sign * A[0, f] * determinant(temp, n - 1);
+
+                // terms are to be added with alternate sign
+                sign = -sign;
+            }
+            return D;
+        }
+
+
+        static void adjoint(double[,] A, double[,] adj)
+        {
+            if (N == 1)
+            {
+                adj[0, 0] = 1;
+                return;
+            }
+
+            // temp is used to store cofactors of [,]A
+            int sign = 1;
+            double[,] temp = new double[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    // Get cofactor of A[i,j]
+                    getCofactor(A, temp, i, j, N);
+
+                    // sign of adj[j,i] positive if sum of row
+                    // and column indexes is even.
+                    sign = ((i + j) % 2 == 0) ? 1 : -1;
+
+                    // Interchanging rows and columns to get the
+                    // transpose of the cofactor matrix
+                    adj[j, i] = (sign) * (determinant(temp, N - 1));
+                }
+            }
+        }
+
+
+        static bool inverse(double[,] A, double[,] inverse)
+        {
+            // Find determinant of [,]A
+            double det = determinant(A, N);
+            if (det == 0)
+            {
+                Console.Write("Singular matrix, can't find its inverse");
+                return false;
+            }
+
+            // Find adjoint
+            double[,] adj = new double[N, N];
+            adjoint(A, adj);
+
+            // Find Inverse using formula "inverse(A) = adj(A)/det(A)"
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    inverse[i, j] = adj[i, j] / (float)det;
+
+            return true;
+        }
+
+
+        static void display(double[,] A)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(A[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+}
